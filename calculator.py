629d5cc51dcbd669538cff63823097c364e57512
Uladzimir Al'khimovich <58719393+vladalh@users.noreply.github.com>
2022-01-04 22:25:31 +0300

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -15,19 +15,4 @@ def result(x, y, sign):
         result = x ** y
     return result
 
-
-try:
-    # Enter a math expression separated by a space
-    exp = input("enter a number, sign and number separated by a space :").split()
-
-    # Getting ahead of the first and second numbers
-    number1 = int(exp[0])
-    number2 = int(exp[2])
-
-    print("{0:.3f}".format(result(number1, number2, exp[1])))
-
-except ZeroDivisionError:
-    print("Division by zero!")
-
-
 # vladalh@mail.ru
